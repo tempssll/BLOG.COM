bb403e0b7700e36c03d13c389bf55463eed4b36a
brayan-coy <brayan324554@gmail.com>
2023-07-31 14:51:41 -0500

    first commit

---
+++ app2.js
@@ -0,0 +1,201 @@
+// Datos simulados de productos para café molido
+const cafeMolido = [
+  {
+    id: 101,
+    name: "CAFÉ MOLIDO",
+
+    image: "cafeuno.jpeg",
+  },
+  {
+    id: 102,
+    name: "CAFÉ MOLIDO",
+
+    image: "cafedos.jpeg",
+  },
+  {
+    id: 101,
+    name: "CAFÉ MOLIDO",
+
+    image: "cafetres.jpeg",
+  },
+
+  {
+    id: 101,
+    name: "CAFÉ MOLIDO",
+
+    image: "cafecuatro.jpeg",
+  },
+
+  {
+    id: 101,
+    name: "CAFÉ MOLIDO",
+
+    image: "cafecinco.jpeg",
+  },
+
+  //SEGUNDA SECCION CAFE
+  {
+    id: 101,
+    name: "CAFÉ MOLIDO",
+
+    image: "cafeseis.jpeg",
+  },
+  {
+    id: 101,
+    name: "CAFÉ MOLIDO",
+
+    image: "cafesiete.jpeg",
+  },
+  {
+    id: 101,
+    name: "CAFÉ MOLIDO",
+
+    image: "cafeocho.jpeg",
+  },
+  {
+    id: 101,
+    name: "CAFÉ MOLIDO",
+
+    image: "cafenueve.jpeg",
+  },
+  {
+    id: 101,
+    name: "CAFÉ MOLIDO",
+    price: 9000,
+    image: "cafedies.jpeg",
+  },
+];
+
+//************************************************************************** */
+
+// Datos simulados de productos para café en grano
+const cafeEnGrano = [
+  {
+    id: 201,
+    name: "CAFÉ EN GRANO",
+    image: "cafegranouno.jpeg",
+  },
+  {
+    id: 202,
+    name: "CAFÉ EN GRANO",
+    image: "cafegranodos.jpeg",
+  },
+
+  {
+    id: 202,
+    name: "CAFÉ EN GRANO",
+    image: "cafegranotres.jpeg",
+  },
+  {
+    id: 202,
+    name: "CAFÉ EN GRANO",
+    image: "cafegranocuatro.jpeg",
+  },
+  {
+    id: 202,
+    name: "CAFÉ EN GRANO",
+    image: "cafecinco.jpeg",
+  },
+  {
+    id: 202,
+    name: "CAFÉ EN GRANO",
+    image: "cafegranoseis.jpeg",
+  },
+  {
+    id: 202,
+    name: "CAFÉ EN GRANO",
+    image: "cafegranosiete.jpeg",
+  },
+  {
+    id: 202,
+    name: "CAFÉ EN GRANO",
+    image: "cafegranoocho.jpeg",
+  },
+  {
+    id: 202,
+    name: "CAFÉ EN GRANO",
+    image: "cafegranonueve.jpeg",
+  },
+  {
+    id: 202,
+    name: "CAFÉ EN GRANO",
+    image: "cafegranouno.jpeg",
+  },
+
+  // Agrega más productos de café en grano aquí
+];
+
+// Función para cargar imágenes dinámicamente en una sección
+function cargarImagenesEnSeccion(seccion, productos) {
+  const contenedor = document.querySelector(seccion);
+
+  productos.forEach((producto) => {
+    const productItem = document.createElement("div");
+    productItem.classList.add("product-item");
+
+    const productImage = document.createElement("img");
+    productImage.src = `imagenes/${producto.image}`;
+    productImage.alt = producto.name;
+    productImage.classList.add("product-image");
+    productItem.appendChild(productImage);
+
+    const productName = document.createElement("div");
+    productName.textContent = producto.name;
+    productName.classList.add("product-name");
+    productItem.appendChild(productName);
+
+    const productPrice = document.createElement("div");
+    productPrice.textContent = `$${producto.price}`;
+    productPrice.classList.add("product-price");
+    productItem.appendChild(productPrice);
+
+    contenedor.appendChild(productItem);
+  });
+}
+
+// Cargar imágenes de café molido en la sección "Café molido"
+cargarImagenesEnSeccion("#cafe-molido-section", cafeMolido);
+
+// Cargar imágenes de café en grano en la sección "Café en Grano"
+cargarImagenesEnSeccion("#cafe-en-grano-section", cafeEnGrano);
+
+//REPRODUCIR EL VIDEO
+// Función para cargar el video de manera automática y reproducirlo
+function cargarVideo() {
+  const videoElement = document.getElementById("video");
+  videoElement.play();
+}
+
+// Llama a la función para cargar y reproducir el video al cargar la página
+cargarVideo();
+
+// JavaScript (app.js)
+document.addEventListener("DOMContentLoaded", function () {
+  const videoElement = document.getElementById("video");
+  videoElement.play();
+});
+
+/* seccion boton despliegue  de boton*/
+function toggleProductos() {
+  var productosSection = document.getElementById("productosSection");
+  if (productosSection.style.display === "none") {
+    productosSection.style.display = "block";
+  } else {
+    productosSection.style.display = "none";
+  }
+}
+
+toggleProducto();
+
+// Agregamos la clase "productods-fixed" al botón cuando alcance cierta posición
+window.onscroll = function () {
+  var button = document.querySelector(".productods");
+  var distanciaDesdeArriba = button.offsetTop;
+  var scrollY = window.scrollY || window.pageYOffset;
+
+  if (scrollY > distanciaDesdeArriba) {
+    button.classList.add("productods-fixed");
+  } else {
+    button.classList.remove("productods-fixed");
+  }
+};
---
+++ script.js
@@ -0,0 +1,34 @@
+// Función para ocultar el splash y mostrar el contenido principal después de cargar todo
+window.addEventListener("load", function () {
+  const splashScreen = document.getElementById("splash-screen");
+  const mainContent = document.getElementById("main-content");
+
+  // Espera 1.5 segundos antes de ocultar el splash y mostrar el contenido principal
+  setTimeout(function () {
+    splashScreen.style.display = "none";
+    mainContent.style.display = "block";
+  }, 10); // Cambia este valor para ajustar el tiempo de espera
+
+  // Agregar evento de clic al contenido principal para cerrar la barra lateral
+  mainContent.addEventListener("click", function (event) {
+    const sidebar = document.getElementById("sidebar");
+    const toggleButton = document.getElementById("toggleButton");
+
+    // Verificar si el clic proviene del botón VER o la barra lateral
+    if (event.target !== toggleButton && event.target !== sidebar) {
+      sidebar.style.left = "-250px";
+    }
+  });
+});
+
+// Función para mostrar/ocultar el menú al hacer clic en el botón VER
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.style.left = sidebar.style.left === "0px" ? "-250px" : "0px";
+}
+
+// Función para mostrar el contenido del menú seleccionado
+function showMenuContent(menuItem) {
+  const menuContent = document.getElementById("menu-content");
+  menuContent.innerHTML = `Has seleccionado ${menuItem}. Aquí se mostraría el contenido correspondiente al menú.`;
+}
