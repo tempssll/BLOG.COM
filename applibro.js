73f29c9e795076dfcb0e5d83d96305bb68eb0a7f
brayan-coy <brayan324554@gmail.com>
2023-09-06 12:53:37 -0500

    first commit

--- applibro.js
+++ applibro.js
@@ -1,4 +1,4 @@
-// Obtén el botón por su id y tambien habre el libro mio en otra ventana y lo redirige
+/* // Obtén el botón por su id y tambien habre el libro mio en otra ventana y lo redirige
 var btnEnlace = document.getElementById("enlaceLibro");
 
 // Agrega un evento de clic al botón
@@ -6,3 +6,4 @@ btnEnlace.addEventListener("click", function () {
   // Abre una nueva ventana
   window.open("/iconow/BLOG BRAYHAN ESTIVEN COY RUBIO/index.html", "_blank");
 });
+ */
