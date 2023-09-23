d64b284b9cb4987d953975aea1c3a26b38a7ad2a
brayan-coy <brayan324554@gmail.com>
2023-09-22 19:24:18 -0500

    first commit

---
+++ credito.js
@@ -0,0 +1,5 @@
+// Activar la animación de créditos después de cargar la página
+document.addEventListener("DOMContentLoaded", function () {
+  const creditos = document.querySelector(".creditos");
+  creditos.classList.add("activo");
+});
