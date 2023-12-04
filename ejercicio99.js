82b5251c537b4556cc978cf1b4c94c6f8595fe8c
brayan-coy <brayan324554@gmail.com>
2023-12-04 13:16:58 -0500

    first commit

--- ejercicio99.js
+++ ejercicio99.js
@@ -1,6 +1,5 @@
 
 /* Un objeto es una colección de propiedades, y una propiedad es una asociación entre un nombre (o clave) y un valor.  */
-
 const producto = {
   nombre: 'manzana',
   categoria: 'frutas',
